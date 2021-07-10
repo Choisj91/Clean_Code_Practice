0fa04d0e6322758e489521c8e7aa521212b38c48
SeungJae <stopj1123@gmail.com>
2021-07-10 23:10:54 +0900

    naming-exceptions Examples

--- good_name.py
+++ good_name.py
@@ -1,1 +1,10 @@
-print('Hello Dear')
+from datetime import datetime
+
+class DateUtil:
+    @staticmethod
+    def get_formatted_today():
+        date_today = datetime.now()
+        formatted_date = date_today.strftime('%Y-%m-%d')
+        return formatted_date
+
+print(DateUtil.get_formatted_today())
